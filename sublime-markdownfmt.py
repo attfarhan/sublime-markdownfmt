4c80a87ecf06dcc2276fcb7ce09a9c95f912783e
Farhan Attamimi <farhan.attamimi@gmail.com>
2016-08-20 10:47:26 +0700

    Filter for .md files only

--- sublime-markdownfmt.py
+++ sublime-markdownfmt.py
@@ -3,6 +3,9 @@ import sublime, sublime_plugin, subprocess, os
 class SublimeMarkdownfmt(sublime_plugin.EventListener): 
 	def on_pre_save(self, view): 
 		print ("active view ", sublime.View.file_name(view))
-		command = ["/Users/Farhan/GoWork/bin/markdownfmt", "-w", sublime.View.file_name(view)]
-		print(command)
-		subprocess.Popen(command, env=os.environ, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+		filename = sublime.View.file_name(view)
+		filename_sans_ext, fileext = os.path.splitext(sublime.View.file_name(view))
+		if fileext == '.md':
+			command = ["/Users/Farhan/GoWork/bin/markdownfmt", "-w", filename]
+			print(command)
+			subprocess.Popen(command, env=os.environ, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
